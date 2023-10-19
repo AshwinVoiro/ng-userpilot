25ddf9beb991ac0841e556b7d7ded0e9e1966a16
AshwinVoiro <ashwin@voiro.com>
2023-10-19 14:03:16 +0530

    initial commit

---
+++ lib/index.d.ts
@@ -0,0 +1,119 @@
+// userpilot.js
+
+/**
+ * Init
+ */
+export function initialize(str) {
+    return str;
+}
+
+/**
+ * Reload
+ * @param url
+ */
+export function reload(url) {
+    // Implement the function logic here
+}
+
+/**
+ * Identify
+ * @param userid
+ * @param parameters
+ */
+export function identify(userid, parameters) {
+    // Implement the function logic here
+}
+
+/**
+ * Suppress
+ */
+export function suppress() {
+    // Implement the function logic here
+}
+
+/**
+ * Trigger
+ * @param token
+ */
+export function trigger(token) {
+    // Implement the function logic here
+}
+
+/**
+ * Anonymous
+ * @param data
+ */
+export function anonymous(data) {
+    // Implement the function logic here
+}
+
+/**
+ * On
+ * @param event
+ * @param cb
+ */
+export function on(event, cb) {
+    // Implement the function logic here
+}
+
+/**
+ * Once
+ * @param event
+ * @param cb
+ */
+export function once(event, cb) {
+    // Implement the function logic here
+}
+
+/**
+ * Off
+ * @param event
+ */
+export function off(event) {
+    // Implement the function logic here
+}
+
+/**
+ * Track
+ * @param event
+ * @param meta
+ */
+export function track(event, meta) {
+    // Implement the function logic here
+}
+
+/**
+ * Reset
+ */
+export function reset() {
+    // Implement the function logic here
+}
+
+/**
+ * Next
+ */
+export function next() {
+    // Implement the function logic here
+}
+
+/**
+ * End
+ * @param type
+ */
+export function end(type) {
+    // Implement the function logic here
+}
+
+/**
+ * Clean
+ */
+export function clean() {
+    // Implement the function logic here
+}
+
+/**
+ * Destroy
+ */
+export function destroy() {
+    // Implement the function logic here
+}
